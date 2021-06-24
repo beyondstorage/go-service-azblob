6cbff2163c8582bf65099e6d42492da64858213f
JinnyYi <jinnyyi@yunify.com>
2021-06-24 18:04:46 +0800

    Fix direr test failed

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -2,27 +2,26 @@ package tests
 
 import (
 	"os"
-	"strings"
 	"testing"
 
-	"github.com/google/uuid"
-
-	_ "github.com/beyondstorage/go-service-azblob/v2"
-	"github.com/beyondstorage/go-storage/v4/services"
+	azblob "github.com/beyondstorage/go-service-azblob/v2"
+	ps "github.com/beyondstorage/go-storage/v4/pairs"
 	"github.com/beyondstorage/go-storage/v4/types"
+	"github.com/google/uuid"
 )
 
 func setupTest(t *testing.T) types.Storager {
 	t.Log("Setup test for azblob")
 
-	var connStr strings.Builder
-	connStr.WriteString("azblob://")
-	connStr.WriteString(os.Getenv("STORAGE_AZBLOB_NAME"))
-	connStr.WriteString("/" + uuid.New().String())
-	connStr.WriteString("?credential=" + os.Getenv("STORAGE_AZBLOB_CREDENTIAL"))
-	connStr.WriteString("&endpoint=" + os.Getenv("STORAGE_AZBLOB_ENDPOINT"))
-
-	store, err := services.NewStoragerFromString(connStr.String())
+	store, err := azblob.NewStorager(
+		ps.WithCredential(os.Getenv("STORAGE_AZBLOB_CREDENTIAL")),
+		ps.WithName(os.Getenv("STORAGE_AZBLOB_NAME")),
+		ps.WithEndpoint(os.Getenv("STORAGE_AZBLOB_ENDPOINT")),
+		ps.WithWorkDir("/"+uuid.New().String()+"/"),
+		azblob.WithStorageFeatures(azblob.StorageFeatures{
+			VirtualDir: true,
+		}),
+	)
 	if err != nil {
 		t.Errorf("new storager: %v", err)
 	}
